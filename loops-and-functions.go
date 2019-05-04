ac49575510b8ac6f7b28d708fc8a56d40fc657f1
Brian.Bian <Brian.Xb.Bian@gmail.com>
2019-05-04 21:46:54 +0800

    first commit

---
+++ loops-and-functions.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for math.Abs(z*z-x) > 0.00001 {
+		z = (z + x/z) / 2
+	}
+
+	return z
+
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}
