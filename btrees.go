0c72030f2e3b2526200b7e1c0dcf7e11d17379da
Brian.Bian <Brian.Xb.Bian@gmail.com>
2019-05-08 23:00:17 +0800

    Update

---
+++ btrees.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	Walk(t.Left, ch)
+	ch <- t.Value
+	Walk(t.Right, ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+// func Same(t1, t2 *tree.Tree) bool {
+// 	ch1, ch2 := make(chan int), make(chan int)
+// 	go Walk(t1, ch1)
+// 	go Walk(t2, ch2)
+// 	for {
+// 		v1, ok1 := <-ch1
+// 		v2, ok2 := <-ch2
+// 		switch {
+// 		case !ok1, !ok2:
+// 			return ok1 == ok2
+// 		case v1 != v2:
+// 			return false
+// 		}
+// 	}
+// }
+
+func main() {
+	ch := make(chan int)
+	go Walk(tree.New(1), ch)
+	for v := range ch {
+		fmt.Println(v)
+	}
+
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+}
--- rot13.go
+++ rot13.go
@@ -6,16 +6,28 @@ import (
 	"strings"
 )
 
-func rot13(b byte) byte {
-
-}
-
 type rot13Reader struct {
 	r io.Reader
 }
 
-func Read(r rot13Reader) (int, error) {
+func rot13(b byte) byte {
+	switch {
+	case 'a' <= b && b <= 'm' || 'A' <= b && b <= 'M':
+		return b + 13
+	case 'n' <= b && b <= 'z' || 'N' <= b && b <= 'Z':
+		return b - 13
+	default:
+		return b
+	}
+}
 
+func (r rot13Reader) Read(p []byte) (n int, err error) {
+	n, err = r.r.Read(p)
+	for i := 0; i < n; i++ {
+		p[i] = rot13(p[i])
+	}
+
+	return
 }
 
 func main() {
