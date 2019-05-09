09a19cecf4d21e3464b05088e128f8170d706167
Brian.Bian <Brian.Xb.Bian@gmail.com>
2019-05-09 10:40:51 +0800

    Update

--- btrees.go
+++ btrees.go
@@ -9,31 +9,36 @@ import (
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
+	walk(t, ch)
+	close(ch)
+}
+
+func walk(t *tree.Tree, ch chan int) {
 	if t == nil {
 		return
 	}
-	Walk(t.Left, ch)
+	walk(t.Left, ch)
 	ch <- t.Value
-	Walk(t.Right, ch)
+	walk(t.Right, ch)
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-// func Same(t1, t2 *tree.Tree) bool {
-// 	ch1, ch2 := make(chan int), make(chan int)
-// 	go Walk(t1, ch1)
-// 	go Walk(t2, ch2)
-// 	for {
-// 		v1, ok1 := <-ch1
-// 		v2, ok2 := <-ch2
-// 		switch {
-// 		case !ok1, !ok2:
-// 			return ok1 == ok2
-// 		case v1 != v2:
-// 			return false
-// 		}
-// 	}
-// }
+func Same(t1, t2 *tree.Tree) bool {
+	ch1, ch2 := make(chan int), make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+		switch {
+		case !ok1, !ok2:
+			return ok1 == ok2
+		case v1 != v2:
+			return false
+		}
+	}
+}
 
 func main() {
 	ch := make(chan int)
